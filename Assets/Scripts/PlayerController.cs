aafebc4271c8ee49da25b87ff30ecd2412eb7537
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-04-26 16:22:17 +0300

    Change Player Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(1f, 0f);
+    }
+}
