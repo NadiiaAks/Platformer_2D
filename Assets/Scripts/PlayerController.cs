86bc2373035dbeea74d360a7cd25cca20a31e86e
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-04-27 17:02:39 +0300

    Add jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speedX = -1f;
 
     private float horizontal = 0f;
+
+    private bool isGround = false;
+    private bool isJump = false;
     
     private Rigidbody2D rb;
 
@@ -20,10 +23,28 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
+        if(Input.GetKey(KeyCode.W) && isGround)
+        {
+            isJump = true;
+        }
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * speedX * speedXMultiplayer * Time.fixedDeltaTime, rb.velocity.y);
+        if (isJump)
+        {
+            rb.AddForce(new Vector2(0f, 400f));
+            isGround = false;
+            isJump = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
     }
 }
