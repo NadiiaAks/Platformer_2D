6ed0069c5d5850573e998eca49d015882c3d9591
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-05-06 23:15:51 +0300

    Adding territory patrol for enemy

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -12,22 +12,74 @@ public class EnemyController : MonoBehaviour
     private Vector2 _leftPosition;
     private Vector2 _rightPosition;
 
+    private bool _isFacingRight = true;
+    private bool _isWait = false;
+    private float _waitTime;
+
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
 
         _leftPosition = transform.position;
         _rightPosition = _leftPosition + Vector2.right * walkDistance;
+        _waitTime = timeToWait;
+    }
+
+    private void Update()
+    {
+        if(_isWait)
+        {
+            Wait();
+        }
+        
+        bool isOutOfRightBoundary = _isFacingRight && transform.position.x >= _rightPosition.x;
+        bool isOutOfLeftBoundary = !_isFacingRight && transform.position.x <= _leftPosition.x;
+
+        if(isOutOfRightBoundary || isOutOfLeftBoundary)
+        {
+            _isWait = true;
+        }
     }
 
     private void FixedUpdate()
     {
-        _rb.MovePosition((Vector2)transform.position + Vector2.right * walkSpeed * Time.fixedDeltaTime);
+        Vector2 nextPoint = Vector2.right * walkSpeed * Time.fixedDeltaTime;
+
+        if (!_isFacingRight)
+        {
+            nextPoint.x *= -1;
+        }
+        
+        if (!_isWait)
+        {
+            _rb.MovePosition((Vector2)transform.position + nextPoint);
+        }
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawLine(_leftPosition, _rightPosition);
+    }
+
+   //waiting (timer code)
+   private void Wait ()
+    {
+        _waitTime -= Time.deltaTime;
+        if(_waitTime < 0f)
+        {
+            _waitTime = timeToWait;
+            _isWait = false;
+            Flip();
+        }
+    }
+    
+    //flipping character
+    private void Flip()
+    {
+        _isFacingRight = !_isFacingRight;
+        Vector3 enemyFlip = transform.localScale;
+        enemyFlip.x *= (-1);
+        transform.localScale = enemyFlip;
     }
 }
