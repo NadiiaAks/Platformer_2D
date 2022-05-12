8c5a29ef74f472427f855f454a8a3bc68d6cfe1c
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-05-12 13:08:02 +0300

    Adding attack controller and attack animation

---
+++ Assets/Scripts/AttackController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackController : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    private bool _isAttack;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _isAttack = true;
+            animator.SetTrigger("attack");
+        }
+    }
+}
